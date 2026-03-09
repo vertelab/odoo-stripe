7123f05f367fddb0730bb0ef92b4de710f83de89
Marcus Jakobsson <marcus.jakobsson@vertel.se>
2026-03-09 10:28:03 +0000

    readded view

--- stripe_processing_fees/__manifest__.p.py
+++ stripe_processing_fees/__manifest__.p.py
@@ -10,6 +10,7 @@
     'depends': ['payment_stripe', 'payment', 'account', 'account_payment'],
     'data': [
         'views/payment_views.xml',
+        'views/payment_provider_views.xml',
     ],
     'installable': True,
     'application': True,
